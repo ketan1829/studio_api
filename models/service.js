b3a6fa605b9b438fe0d493cdc0f341bb074dfae5
uday <uday.singh@choira.io>
2024-03-04 20:54:25 +0530

    updated create sercice and joi validation for packages

--- models/service.js
+++ models/service.js
@@ -5,10 +5,10 @@ const { getDB } = require('../util/database');
 const collectionName = 'services';
 
 class Service {
-    constructor(service_id, fullName, type, price, amenities, totalPlans, packages, servicePhotos, aboutUs, workDetails, discographyDetails, clientPhotos, reviews, featuredReviews, isActive) {
+    constructor(service_id, fullName, price, amenities, totalPlans, packages,
+        servicePhotos, aboutUs, workDetails, discographyDetails, clientPhotos, reviews, featuredReviews,isActive,type) {
         this.service_id = service_id;
         this.fullName = fullName;
-        this.type = type;
         this.price = price; // Starting price of plan
         this.amenities = amenities; // Array of objects like [{id:"",name:""},{..},....]
         this.totalPlans = totalPlans;
@@ -16,22 +16,62 @@ class Service {
         this.servicePhotos = servicePhotos; // Array of strings(image URLs)
         this.aboutUs = aboutUs; // Array of Object
         this.workDetails = workDetails; // Array of objects like [{name:"", designation:"", imgUrl:""},{....},....]
-        this.clientPhotos = clientPhotos;
         this.discographyDetails = discographyDetails;
+        this.clientPhotos = clientPhotos;
         this.reviews = reviews; // Array of Objects
         this.featuredReviews = featuredReviews; // Array of Objects
         this.isActive = isActive; // 0-> No, 1-> Yes
+        this.type = type;
         this.creationTimeStamp = new Date();
     }
 
-    async save() {
+  
+
+
+    async checkBeforeSave() {
         const db = getDB();
         try {
+            // const existingService = await db.collection(collectionName).findOne({ service_id: this.service_id });
+            
+            // if (existingService) {
+            //     return { status: false, message: "Service with the given ID already exists" };
+            // }
+    
             const result = await db.collection(collectionName).insertOne(this);
             return result.ops[0];
         } catch (error) {
             console.error('Error saving service:', error);
             throw error;
+        }
+    }
+    
+
+    static async deleteServiceById(sId) {
+        const db = getDB();
+    
+        try {
+            
+            const existingService = await db.collection(collectionName).findOneAndDelete({ _id: new ObjectId(sId) });
+            console.log(existingService);
+            if (!existingService) {
+                return { status: false, message: "No Service with this ID exists" }
+            }
+    
+            return{ status: true, message: "Service deleted successfully" };
+        } catch (error) {
+            console.error("Error deleting service:", error);
+            return{ status: false, message: "Internal Server Error" };
+        }
+    }
+    static async updateServiceById(sId,newData) {
+        const db = getDB();
+    
+        try {            
+             const updatedResult = await db.collection(collectionName).findOneAndUpdate({ _id: new ObjectId(sId) },{$set: newData},{new:true});                
+            return{ status: true, message: "Service updated successfully", updatedService:updatedResult };
+        } catch (error) {
+            console.error("Error deleting service:", error);
+            return{ status: false, message: "Internal Server Error" };
         }
     }
 
