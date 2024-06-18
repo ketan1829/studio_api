807bdf4a16831828e5013f2921980ab61715bd0b
NitinNi3 <nitin.goswami@choira.io>
2024-06-18 13:01:03 +0000

    added status in pick

--- util/pick.js
+++ util/pick.js
@@ -11,7 +11,11 @@
     return keys.reduce((obj, key) => {
       if (object && Object.prototype.hasOwnProperty.call(object, key)) {
         // eslint-disable-next-line no-param-reassign
-        obj[key] = object[key];
+        if(key === "status"){
+          obj[key] = parseInt(object[key])
+        }else{
+          obj[key] = object[key];
+        }
       }
       return obj;
     }, {});
