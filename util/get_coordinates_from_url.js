cc9e2f3375161d2d495ebeb455141b9b099a01e5
NitinNi3 <nitin.goswami@choira.io>
2024-07-08 15:37:00 +0000

    added latlong from url in studio creation

---
+++ util/get_coordinates_from_url.js
@@ -0,0 +1,20 @@
+
+
+const getLatLong = async(googleMapsUrl) => {
+  try {
+    // Fetch the URL content
+    const response = await axios.get(googleMapsUrl);
+    const redirectUrl = response.request.res.responseUrl;
+    const latlong = redirectUrl.split("/").splice(-1)[0].split("?")[0].split(",")
+    const lat = latlong[0].replace("+","").replace("-","");
+    const long = latlong[1].replace("+","").replace("-","");
+    return [lat,long]
+    
+  } catch (error) {
+    console.error('Error fetching coordinates:', error);
+    return null;
+  }
+}
+
+exports.getLatLong = getLatLong;
+
