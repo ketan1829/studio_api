9ce3699977297c377be325ed040fdfb5f7ef49ed
uday <uday.singh@choira.io>
2024-07-12 19:37:50 +0530

    created - (validation) - created schema type validation for create and update studio

--- util/validations.js
+++ util/validations.js
@@ -73,55 +73,81 @@ const ServicefilterSchema = Joi.object({
   creationTimeStamp: Joi.string(),
 });
 
-//dummy code for studio validation
-// const studioCreateSchema = Joi.object({
-//   fullName:Joi.string(),
-//   address:Joi.string(),
-//   mapLink:Joi.string(),
-//   city:Joi.string(),
-//   state:Joi.string(),
-//   area:Joi.number(),
-//   pincode:Joi.string(),
-//   pricePerHour:Joi.number(),
-//   availabilities:Joi.array().items(Joi.object({
-//     endTime:Joi.string(),
-//     startTime:Joi.string()
-//   })),
-//   amenities:Joi.array().items(
-//     Joi.object({id:Joi.string(),name:Joi.string()}),
-//     Joi.object({id:Joi.string(),name:Joi.string()}),
-//     Joi.object({id:Joi.string(),name:Joi.string()}),
-//     Joi.object({id:Joi.string(),name:Joi.string()}),
-// ),
-//   totalRooms:Joi.number(),
-//   roomsDetails:Joi.array(),
-//   maxGuests:Joi.string(),
-//   studioPhotos:Joi.array(),
-//   aboutUs:Joi.object({
-//     aboutUs:Joi.string(),
-//     services:Joi.string(),
-//     infrastructure:Joi.string()
-//   }),
-//   teamDetails:Joi.array().items(Joi.object({
-//     imgUrl:Joi.string(),
-//     name:Joi.string(),
-//     designation:Joi.string(),
-//     id:Joi.number()
-//   })),
-//   clientPhotos:Joi.array(),
-//   reviews:Joi.object({
-//     avgService:Joi.any(),
-//     avgStudio:Joi.any(),
-//     avgAmenity:Joi.any(),
-//     avgLocation:Joi.any(),
 
-//   }),
-//   featuredReviews:Joi.array(),
-//   country:Joi.string()
-// })
+const studioSchema = Joi.object({
+  fullName:Joi.string().required(),
+  address:Joi.string().required(),
+  mapLink:Joi.string().required(),
+  city:Joi.string().required(),
+  state:Joi.string().required(),
+  area:Joi.number().strict().required(),
+  pincode:Joi.string().required(),
+  pricePerHour:Joi.number().strict().required(),
+  availabilities:Joi.array().items(Joi.object({
+    endTime:Joi.string().required(),
+    startTime:Joi.string().required()
+  })).min(1).required(),
+  
+  amenities:Joi.array().items(
+    Joi.object({id:Joi.string(),name:Joi.string()}).required(),
+).min(1).required(),
+  totalRooms:Joi.number().strict().required(),
+  roomsDetails:Joi.array().items(
+    Joi.object({
+      roomId:Joi.number().strict(),
+      roomName:Joi.string(),
+      area:Joi.string(),
+      details:Joi.array(),
+      amenities:Joi.array(),
+      basePrice:Joi.number().strict(),
+      discountPercentage:Joi.number().strict(),
+      generalStartTime:Joi.string(),
+      generalEndTime:Joi.string(),
+      // availabilities:Joi.array().items(Joi.object({endTime:Joi.string(),startTime:Joi.string()})),
+      availabilities:Joi.array().items(Joi.object({
+        endTime:Joi.string().required(),
+        startTime:Joi.string().required()
+      })),
+      generalTime:Joi.object({
+        endTime:Joi.string(),
+        startTime:Joi.string(),
+      }),
+      roomPhotos:Joi.array(),
+      bookingDays:Joi.array().items(
+        Joi.object({id:Joi.number().strict(),name:Joi.string()}),
+      ),
+      pricePerHour:Joi.number().strict()
+    }).required()
+  ).min(1).required(),
+  maxGuests:Joi.string().required(),
+  studioPhotos:Joi.array().required(),
+  aboutUs:Joi.object({
+    aboutUs:Joi.string().required(),
+    services:Joi.string(),
+    infrastructure:Joi.string(),
+  }).min(1).required(),
+  teamDetails:Joi.array().items(Joi.object({
+    imgUrl:Joi.string(),
+    name:Joi.string().required(),
+    designation:Joi.string(),
+    id:Joi.number().strict()
+  })).min(1).required(),
+
+  clientPhotos:Joi.array(),
+  reviews:Joi.object({
+    avgService:Joi.number(),
+    avgStudio:Joi.number(),
+    avgAmenity:Joi.number(),
+    avgLocation:Joi.number(),
+  }),
+  overallAvgRating:Joi.number(),
+  featuredReviews:Joi.array(),
+  country:Joi.string().required()
+})
 
 module.exports = {
   validateService: (data) => serviceSchema.validate(data),
   validateFilterSchema: (data) => filterSchema.validate(data),
   validateServiceFilterSchema: (data) => ServicefilterSchema.validate(data),
+  studioCreateSchema:(data)=> studioSchema.validate(data)
 };
